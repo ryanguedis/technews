12b2ffcab38ce999f9cb95c20034a44279ced74c
Ryan Guedes <ryanguedesbarradas@gmail.com>
2025-11-10 19:05:45 +0000

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+function About() {
+    return <h1> Estou treinando as rotas do meu site! </h1>; 
+}
+function Description() {
+    return <p> Eu estou ficando muito bom nisso! </p>;
+}
+
+export default About, Description;
